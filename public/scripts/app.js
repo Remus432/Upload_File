b0947e36fc54b363e753d62cb5dd90e1d3d7c25b
Remus432 <buhaianu_remus@yahoo.com>
2018-02-09 14:52:48 +0200

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,12 @@ const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/toporasca/upload"
 const CLOUDINARY_UPLOAD_PRESET = "ub4dd39l";
 
 const imgPreview = document.getElementById("img-preview");
-const videoPreview = document.querySelector("audio");
+const videoPreview = document.querySelector("video");
 const fileUpload = document.getElementById("file-upload");
 
 fileUpload.addEventListener("change", e => {
-    let file = event.target.files[0];
+    var file = event.target.files[0];
+    console.log(file);
     let formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
@@ -19,8 +20,19 @@ fileUpload.addEventListener("change", e => {
         },
         data: formData
     }).then(res => {
+        if(file.type === "image/png") {
+            imgPreview.src = res.data.secure_url;
+        }
+        if(file.type === "image/jpeg") {
+            imgPreview.src = res.data.secure_url;
+        }
+        if(file.type === "image/gif") {
+            imgPreview.src = res.data.secure_url;
+        }
         console.log(res);
-        videoPreview.src = res.data.secure_url;
+        if(file.type === "video/mp4") {
+            videoPreview.src = res.data.secure_url;
+        }
     }).catch(err => {
         console.log(err);
     });
